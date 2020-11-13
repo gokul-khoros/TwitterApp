5dcaf96b40a63b89db71344befedf2231d19181a
Nandhini-khoros <snandhini499@gmail.com>
2020-11-13 19:50:36 +0530

    Get one user timeline,send direct messages methods added

--- src/main/java/controller/TwitterApp.java
+++ src/main/java/controller/TwitterApp.java
@@ -8,19 +8,18 @@ import java.util.Scanner;
 
 public class TwitterApp {
 
-/*
+
     public Twitter getTwitterInstance() {
         ConfigurationBuilder cb = new ConfigurationBuilder();
         cb.setDebugEnabled(true)
-                .setOAuthConsumerKey("**")
-                .setOAuthConsumerSecret("**")
-                .setOAuthAccessToken("**")
-                .setOAuthAccessTokenSecret("***");
-        TwitterFactory tf = new TwitterFactory();
+                .setOAuthConsumerKey("67svDTgdgUQNwZwgfxSBQ8iSM")
+                .setOAuthConsumerSecret("qmsbwibWKD9N1OVJV0n0VI7Pi5dZ4GAIvQY7CmNRoHWfKEq3kW")
+                .setOAuthAccessToken("1326754818174545920-UyZeQNw9gMDSkdFYszkB0ZPX9k43Nw")
+                .setOAuthAccessTokenSecret("kTSED6Ggmr5hal761veXPIJvRXv79UexbNQdT7cGAUHGI");
+        TwitterFactory tf = new TwitterFactory(cb.build());
         Twitter twitter = tf.getInstance();
         return twitter;
-  git   }
-*/
+    }
 
     void getTimeline(Twitter twitter) throws TwitterException {
 
@@ -30,16 +29,23 @@ public class TwitterApp {
         }
     }
 
+
     void postTweet(Twitter twitter, String msg) throws TwitterException {
 
         Status status = twitter.updateStatus(msg);
         System.out.println("POst tweeted");
     }
-
-    void sendmessage(Twitter twitter,String msg) throws TwitterException {
+    void sendMessage(Twitter twitter,String msg) throws TwitterException {
         String s="tweetbi79876331";
         DirectMessage message = twitter.sendDirectMessage(s, msg);
-        System.out.println("Sent: " +message.getText() + " to @" + message.getRecipientScreenName());
+        System.out.println("Sent: " +message.getText() + " to @" + message.getRecipientId());
+    }
+    void getOneUserTimeline(Twitter twitter , String userId) throws TwitterException {
+
+        List<Status> statuses = twitter.getUserTimeline(userId);
+        for (Status st : statuses) {
+            System.out.println(st.getUser().getName() + "------" + st.getText()+"\n");
+        }
     }
     public static void main(String[] args) throws TwitterException {
 
@@ -47,12 +53,14 @@ public class TwitterApp {
         System.out.println("Enter the message to be posted");
         String st = sc.nextLine();
         String tt = sc.nextLine();
+        String t = sc.nextLine();
         TwitterApp td = new TwitterApp();
         Twitter d = td.getTwitterInstance();
 
         td.postTweet(d,st);
         td.getTimeline(d);
-        td.sendmessage(d,tt);
+        td.sendMessage(d,tt);
+        td.getOneUserTimeline(d,t);
 
     }
 }
