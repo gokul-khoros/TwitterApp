ce13135546ebf9b9728451d820186e09368fdfa9
Nandhini-khoros <snandhini499@gmail.com>
2020-11-17 14:35:42 +0530

    Exception added using try,catch

--- src/main/java/controller/Twitterapp1.java
+++ src/main/java/controller/Twitterapp1.java
@@ -1,38 +1,46 @@
 package controller;
 
 import twitter4j.*;
-
 import java.util.List;
 import java.util.Scanner;
 
 public class Twitterapp1{
-
-
-
-    public static void postTweet(String msg) throws TwitterException {
+    public static void postTweet(String msg){
 
         Twitter twitter= TwitterFactory.getSingleton();
-        Status status = twitter.updateStatus(msg);
-        System.out.println("POst tweeted");
-    }
-
-
-    
-
-    void sendDirectMessage(String msg, String s) throws TwitterException {
-        Twitter twitter= TwitterFactory.getSingleton();
-        DirectMessage message = twitter.sendDirectMessage(s, msg);
-        System.out.println("Sent: " +message.getText() + " to @" + message.getRecipientId());
-    }
-    void getOneUserTimeline(String userId) throws TwitterException {
-        Twitter twitter= TwitterFactory.getSingleton();
-        List<Status> statuses = twitter.getUserTimeline(userId);
-        for (Status st : statuses) {
-            System.out.println(st.getUser().getName() + "------" + st.getText()+"\n");
+        try {
+            Status status = twitter.updateStatus(msg);
+            System.out.println("POst tweeted");
+        }
+        catch (TwitterException e)
+        {
+            e.printStackTrace();
         }
     }
-
-
+    void sendDirectMessage(String msg, String s)  {
+        Twitter twitter= TwitterFactory.getSingleton();
+        try {
+            DirectMessage message = twitter.sendDirectMessage(s, msg);
+            System.out.println("Sent: " + message.getText() + " to @" + message.getRecipientId());
+        }
+        catch (TwitterException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    void getOneUserTimeline(String userId)  {
+        Twitter twitter= TwitterFactory.getSingleton();
+        try {
+            List<Status> statuses = twitter.getUserTimeline(userId);
+            for (Status st : statuses) {
+                System.out.println(st.getUser().getName() + "------" + st.getText() + "\n");
+            }
+        }
+        catch(TwitterException e)
+            {
+                e.printStackTrace();
+            }
+    }
     public static void main(String[] args) throws TwitterException {
 
         Scanner sc = new Scanner(System.in);
@@ -44,8 +52,6 @@ public class Twitterapp1{
         System.out.println("Enter the user Id");
         String t = sc.nextLine();
         Twitterapp1 td = new Twitterapp1();
-
-
         td.postTweet(st);
         Twitterapp2.getTimeline();
         td.sendDirectMessage(tt,ht);
--- src/main/java/controller/Twitterapp2.java
+++ src/main/java/controller/Twitterapp2.java
@@ -8,12 +8,19 @@ import twitter4j.TwitterFactory;
 import java.util.List;
 
 public class Twitterapp2 {
-    public static void getTimeline() throws TwitterException {
+    public static void getTimeline()  {
 
-        Twitter twitter= TwitterFactory.getSingleton();
-        List<Status> statuses = twitter.getHomeTimeline();
-        for (Status st : statuses) {
-            System.out.println(st.getUser().getName() + "------" + st.getText()+"\n");
+        Twitter twitter = TwitterFactory.getSingleton();
+
+        try {
+            List<Status> statuses = twitter.getHomeTimeline();
+            for (Status st : statuses) {
+                System.out.println(st.getUser().getName() + "------" + st.getText() + "\n");
+            }
+
+        } catch (TwitterException e) {
+            e.printStackTrace();
         }
+
     }
 }
